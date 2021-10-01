ce2a384f92ab08e08b48e117b1eb53e511ba5c16
tiaraRS <74866417+tiaraRS@users.noreply.github.com>
2021-10-01 07:27:27 -0200

    refactoring impuesto por estado

--- src/Totalizador.js
+++ src/Totalizador.js
@@ -1,12 +1,14 @@
+
+function impuestoPorEstado(estado){
+    let impuestos = { CA: 0.0825, UT: 0.0665,NV:0.08, TX:0.0625, AL:0.04, "": 0 };
+    return impuestos[estado];    
+}
+
+
 function calcularTotal(cantidad, precio, estado=""){
     let subTotal = precio * cantidad;
-    let impuestoCA = 0.0825;
-    if (estado == "CA") return subTotal + subTotal * impuestoCA;
-    if (estado == "UT") return subTotal + subTotal * 0.0665;
-    if (estado == "NV") return subTotal + subTotal * 0.08;
-    if (estado == "TX") return subTotal + subTotal * 0.0625;
-    if (estado == "AL") return subTotal + subTotal * 0.04;
-    return subTotal;
+    let impuesto = impuestoPorEstado(estado);
+    return subTotal + subTotal * impuesto;
 }
 
 export default calcularTotal;
